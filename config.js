f304b0605f61c798d44f90e97558baf59b98f515
nhtc78 <94442367+nhtc78@users.noreply.github.com>
2021-11-16 17:15:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 const CONFIG = {
     titleWeb: "BOYS DAY CỦA HỘI VÔ HỌC",
-    introTitle: 'anh giai của hội vô học',
-    introDesc: `Món quà thứ 2 đến từ chị em của hội vô học`,
+    introTitle: 'Gửi các anh giai của hội vô học',
+    introDesc: `Món quà thứ 2
+    đến từ chị em của hội vô học`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng mn đã yêu ngay món quà đầu tiên 🥰',
     desc: 'Phải chăng mn đã say ngay từ lúc thấy món quà nè',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao chúng ta lại cùng tổ <3',
-    btnReply: 'Gửi cho các chị em <3',
+    btnReply: 'Gửi cho các chị em<3',
     reply: 'Do duyên số <3',
     mess: ' Bọn mk biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Mong chờ món quà tiếp theo nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: ''
 }
